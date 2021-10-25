e29fcd3e3ef7bcb3c505276bbcb8faa6f983d9f3
Lebedev Timofei <91500245+pascaluser@users.noreply.github.com>
2021-10-25 16:11:19 +0500

    Add files via upload

---
+++ лаб 1.pas
@@ -0,0 +1,29 @@
+﻿var s,x,a,b,c,c1,c2,c3:integer;f:boolean;
+begin
+  readln(x);
+  while (x<100) or (x>999) do readln(x);
+    a:=x mod 10;
+    x:=x div 10;
+    c:=x mod 10;
+    b:=x div 10;
+     while f=false do
+       if (a=c) or (c=b) or (b=a) then begin
+         readln(x);
+         a:=x mod 10;
+         x:=x div 10;
+         c:=x mod 10;
+         b:=x div 10;
+       end 
+       else f:=true;
+       writeln(a,b,c);
+       writeln(a,c,b);
+       writeln(b,a,c);
+       writeln(b,c,a);
+       writeln(c,a,b);
+       writeln(c,b,a);
+       s:=a+b+c;
+       c1:=max(a,max(b,c));
+       c3:=min(a,min(b,c));
+       c2:=s-c1-c3;
+       writeln(c1,c2,c3);
+end.
---
+++ лаб 2.pas
@@ -0,0 +1,21 @@
+﻿uses crt;
+var a,b,k:integer;
+begin
+  randomize;
+  a:=random(101);
+  writeln('Вводите числа от 1 до 100, пока не угадаете задуманное число');
+  k:=0;
+  repeat 
+    read(b);
+    if k>6 then begin
+    writeln('вы проиграли');
+    break;
+    end
+    else
+    if b>a then writeln('моё число меньше')
+    else if b<a then writeln('моё число больше')
+    else if a=b then writeln('Поздравляю,вы угадали');
+    inc(k);
+  until a=b;
+  writeln('количество попыток=',k,'правильное число=',a); 
+end.
