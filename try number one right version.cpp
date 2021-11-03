f85aba4701b3f2e2593f3f4dedd4604362bfbbd5
Lebedev Timofei <91500245+LebedevTimofey@users.noreply.github.com>
2021-11-03 13:48:17 +0300

    Add files via upload

---
+++ try number one right version.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	char a, b, c, d, v;
+	cout << "Введите пять символов ";
+	cin >> a >> b >> c >> d >> v;
+	cout << v << d << c << b << a;
+}
+
+
